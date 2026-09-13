c2400cb7d62f305bee8b49e17ffe43f4a9f7bdd3
real-Hassan <hassan3162f@gmail.com>
2026-09-13 18:33:36 +0500

    Added navbar logic, and html code

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Lore Archive Initialized.");
+// --- NAVBAR TOGGLE LOGIC ---
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+if (hamburger) {
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active'); // Meaningful DOM manipulation
+    });
+}
